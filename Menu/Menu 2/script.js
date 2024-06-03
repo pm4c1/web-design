8ef7bebe3ad0df4893884971dcb354856e2aa31f
pm4c1 <122862249+pm4c1@users.noreply.github.com>
2024-06-04 08:37:32 +0900

    24.06.04

---
+++ Menu/Menu 2/script.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+    let navList = document.querySelector(".nav > ul");
+
+    navList.addEventListener("mouseover", function () {
+        navList.querySelectorAll(".submenu").forEach(sub => {
+            sub.style.height = "155px";
+        });
+    });
+
+    navList.addEventListener("mouseout", function () {
+        navList.querySelectorAll(".submenu").forEach(sub => {
+            sub.style.height = "0px";
+        });
+    });
+}
